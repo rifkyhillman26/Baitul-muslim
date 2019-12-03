440cb99cd5f1f5f2c38dd4e8484cd6f29ee5f71d
rifkyhilman <rifkyhilman265@gmail.com>
2019-12-03 14:31:33 +0700

    firts repostory

---
+++ index.js
@@ -0,0 +1,10 @@
+var flag = 1
+while (flag <= 20) {
+    console.log(flag + ' - I will become fullstack developer')
+    flag++;
+}
+
+// for (var i = 1; i <= flag; i = i + 1) {
+//     console.log('i love ' + "fullstack")
+// }
+
